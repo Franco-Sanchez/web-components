77a00794fe6c43227eae20f028e1ad4a5998d0f0
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-11-01 22:20:43 -0500

    The card variable now is a private variable

--- src/components/weather-card/weather-card.js
+++ src/components/weather-card/weather-card.js
@@ -6,6 +6,8 @@ template.innerHTML = `
 `;
 
 class WeatherCard extends HTMLElement {
+  #card;
+
   constructor() {
     super();
     this._shadowRoot = this.attachShadow({ mode: 'open' })
@@ -23,13 +25,13 @@ class WeatherCard extends HTMLElement {
   async connectedCallback() {
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.latitude}&lon=${this.longitude}&appid=4315c11d495e4dbe1759c2342bf1e8ac`);
     const data = await response.json();
-    this.card = this._shadowRoot.querySelector(".card-body");
+    this.#card = this._shadowRoot.querySelector(".card-body");
     const townName = document.createElement("p");
     townName.textContent = `Town: ${data.name}`;
-    this.card.appendChild(townName);
+    this.#card.appendChild(townName);
     const temperature = document.createElement("p");
     temperature.textContent = `${parseInt(data.main.temp - 273)} °C`;
-    this.card.appendChild(temperature);
+    this.#card.appendChild(temperature);
   }
 }
 
